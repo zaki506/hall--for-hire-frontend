30c019f6912b13b7b419685a2fe31f1084f0fb28
zaki506 <zaki.munawwar9045@gmail.com>
2024-03-20 13:51:11 +0530

    recaptcha

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,43 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import Carousel from "../components/Carousel";
+import ReCAPTCHA from 'react-google-recaptcha'
+import Button from "../components/Button";
+import { useFormik } from "formik";
+import { useNavigate } from "react-router-dom";
+import { toast, Toaster } from "react-hot-toast";
+import axios from "axios";
 
 const Register = () => {
+  const navigate = useNavigate();
+
+  const myFormik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      mobile: "",
+      address: "",
+      city: "",
+      state: "",
+      postcode: "",
+      password: "",
+    },
+
+    onSubmit: async (values) => {
+      await axios
+        .post("http://localhost:8080/api/user/create", values)
+        .then((res) => {
+			console.log("res", res)
+          toast.success(res?.data?.message)
+		  setTimeout(() => {
+			navigate("/")
+		  }, 2000)
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
+    },
+  });
   return (
     <>
       <Carousel clsName="heroSlider h-[200px]" />
@@ -27,11 +62,139 @@ const Register = () => {
         </div>
 
         <div>
-          <form>
+
+          <form onSubmit={myFormik.handleSubmit}>
             
+          <div>
+              <label>Name</label>
+              <input
+                className="form-control"
+                type="text"
+                id="name"
+                // value={}
+                name="name"
+                onChange={myFormik.handleChange}
+              />
+            </div>
+
+            <div>
+              <label>Email</label>
+              <input
+                className="form-control"
+                type="email"
+                id="email"
+                name="email"
+                // value={}
+                onChange={myFormik.handleChange}
+              />
+            </div>
+            
+
+
+            <div>
+              <label>Phone</label>
+              <input
+                className="form-control"
+                type="Number"
+                id="phone"
+                // value={}
+                name="phone"
+                onChange={myFormik.handleChange}
+              />
+            </div>
+
+            <div>
+              <label>Mobile</label>
+              <input
+                className="form-control"
+                type="Number"
+                id="mobile"
+                name="mobile"
+                // value={}
+                onChange={myFormik.handleChange}
+              />
+            </div>
+           
+
+
+            <div>
+              <label>Address</label>
+              <input
+                className="form-control"
+                type="text"
+                id="address"
+                // value={}
+                name="address"
+                onChange={myFormik.handleChange}
+              />
+            </div>
+
+            <div>
+              <label>City</label>
+              <input
+                className="form-control"
+                type="text"
+                id="city"
+                name="city"
+                // value={}
+                onChange={myFormik.handleChange}
+              />
+            </div>
+           
+
+
+            <div>
+              <label>State</label>
+              <input
+                className="form-control"
+                type="text"
+                id="state"
+                // value={}
+                name="state"
+                onChange={myFormik.handleChange}
+              />
+            </div>
+
+            <div>
+              <label>Postcode</label>
+              <input
+                className="form-control"
+                type="number"
+                id="postcode"
+                name="postcode"
+                // value={}
+                onChange={myFormik.handleChange}
+              />
+            </div>
+           
+
+            <div>
+              <label>Password</label>
+              <input
+                className="form-control"
+                type="password"
+                id="password"
+                name="password"
+                // value={}
+                onChange={myFormik.handleChange}
+              />
+            </div>
+
+            <div>
+              <label>Prove you're human</label>
+              <ReCAPTCHA sitekey={process.env.REACT_APP_SITE_KEY} />
+            </div>
+
+
+            <div>
+              <Button cls="btn btn-primary my-2" text="Register Now" />
+            </div>
+            
+
           </form>
         </div>
       </div>
+      <Toaster/>
     </>
   );
 };
